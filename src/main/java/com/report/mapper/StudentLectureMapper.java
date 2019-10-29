aee9f0fb2d4ed8963ead2c8e6c489607e8ded1ee
seyeonn <ansse417@daum.net>
2019-10-29 16:31:18 +0900

    studentlecturemapper수정

--- src/main/java/com/report/mapper/StudentLectureMapper.java
+++ src/main/java/com/report/mapper/StudentLectureMapper.java
@@ -2,14 +2,10 @@ package com.report.mapper;
 
 import java.util.List;
 
-import com.report.dto.Lecture;
 import com.report.dto.StudentLecture;
 
 public interface StudentLectureMapper {
-	StudentLecture fineOne(int lecture_no);
-	StudentLecture findByLectureName(String lecture_name);
-	List<Lecture> findAll();
-	void insert(Lecture lecture);
-	void update(Lecture lecture);
-	void delete(int lecture_no);
+	StudentLecture findOne(int student_lecture_no);
+	List<StudentLecture> findAll();
+
 }
