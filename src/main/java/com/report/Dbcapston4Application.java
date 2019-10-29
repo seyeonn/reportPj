93432e8f71f8f8b8954988233192919a2ec193a5
kimgyuhee <42735333+kimgyuhee@users.noreply.github.com>
2019-10-29 11:28:34 +0900

    mybatis 프로젝트 생성

---
+++ src/main/java/com/report/Dbcapston4Application.java
@@ -0,0 +1,13 @@
+package com.report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dbcapston4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dbcapston4Application.class, args);
+	}
+
+}
