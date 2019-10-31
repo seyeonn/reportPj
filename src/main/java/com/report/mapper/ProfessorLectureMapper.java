38abd99f415b84f0943aba8a1e1f401e9d313e35
yongdongjung <ehdwnd9238@gmail.com>
2019-10-31 14:40:48 +0900

    StudentNoticeMapper.xml생성

--- src/main/java/com/report/mapper/ProfessorLectureMapper.java
+++ src/main/java/com/report/mapper/ProfessorLectureMapper.java
@@ -3,7 +3,9 @@ package com.report.mapper;
 import java.util.List;
 
 import com.report.dto.ProfessorLecture;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface ProfessorLectureMapper {
 	ProfessorLecture findOne(int professor_lecture_no);
 	List<ProfessorLecture> findAll();
--- src/main/java/com/report/mapper/StudentLectureMapper.java
+++ src/main/java/com/report/mapper/StudentLectureMapper.java
@@ -3,7 +3,9 @@ package com.report.mapper;
 import java.util.List;
 
 import com.report.dto.StudentLecture;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface StudentLectureMapper {
 	StudentLecture findOne(int student_lecture_no);
 	List<StudentLecture> findAll();
