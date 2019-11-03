6576393e8f76a154eaba3a17267295bb1116a4c5
seyeonn <ansse417@daum.net>
2019-11-03 19:15:37 +0900

    조인

--- src/main/java/com/report/mapper/ProfessorLectureMapper.java
+++ src/main/java/com/report/mapper/ProfessorLectureMapper.java
@@ -2,10 +2,13 @@ package com.report.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.report.dto.Lecture;
+import com.report.dto.Professor;
 import com.report.dto.ProfessorLecture;
 
 @Mapper
 public interface ProfessorLectureMapper {
 	ProfessorLecture findOne(int professor_lecture_no);
-	ProfessorLecture findByProfessorNo(int professor_no);
+	ProfessorLecture findByProfessorNo(Professor professor_no);
+	ProfessorLecture findByLectureNo(Lecture lecture_no);
 }
