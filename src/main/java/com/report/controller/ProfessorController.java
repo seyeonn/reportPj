00e750b82c7b688762494e42bd3c99f310cb90c8
kimgyuhee <42735333+kimgyuhee@users.noreply.github.com>
2019-11-18 17:24:19 +0900

    점수 입력 완료!!!!!!!

--- src/main/java/com/report/controller/ProfessorController.java
+++ src/main/java/com/report/controller/ProfessorController.java
@@ -257,17 +257,21 @@ public class ProfessorController {
 		
 		
 		List<Homework> homeworks = homeworkMapper.findNotoiceStudents(notice_no);
+		
+		int sum=0;
 
 		for (int i=0; i < hw_no.length ;++i) {
-			
+			System.out.println("========== update 전 ============");
 			System.out.printf("점수 : %d, 과제번호 :%d\n", grade[i], hw_no[i]);
-			System.out.println("========== update 후 ============");
 			homeworkMapper.gradeUpdate(hw_no[i], grade[i]);
+			sum = sum + grade[i];
 			System.out.printf("점수 : %d, 과제번호 :%d\n", grade[i], hw_no[i]);
+			System.out.println("========== update 후 ============\n\n");
 		}
 
+		System.out.println("친구들 점수 총합 -> "+sum);
 		model.addAttribute("homeworks", homeworks);
-		return "professor/inputscore"; // 학생 게시판 페이지
+		return "redirect:inputscore?notice_no="+notice_no; // 학생 게시판 페이지
 	}
 
 
